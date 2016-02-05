f4e8bc0bad368327b7cd4c30dcee64d366804c28
Gareth Jones <gareth@garethpaul.com>
2016-02-04 20:00:00 -0600

    Add UI tests

--- ScreenshareUITests/ScreenshareUITests.swift
+++ ScreenshareUITests/ScreenshareUITests.swift
@@ -19,6 +19,8 @@ class ScreenshareUITests: XCTestCase {
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
+        
+    
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -29,6 +31,13 @@ class ScreenshareUITests: XCTestCase {
     }
     
     func testExample() {
+        
+        let app = XCUIApplication()
+        let menuBarsQuery = app.menuBars
+        menuBarsQuery.menuBarItems["Screenshare"].click()
+        menuBarsQuery.menuItems["About Screenshare"].click()
+        app.buttons[XCUIIdentifierCloseWindow].click()
+        
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
